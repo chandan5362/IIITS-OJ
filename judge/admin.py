049b7bf3cd63226b3044e74e8b1e9be49503bfc2
chandan5362 <chandanraj559@gmail.com>
2020-07-22 20:51:14 +0530

    It is a group project where I collaborated with other 4 members and created this awesome coding platform.
    Final commit

---
+++ judge/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Problem,Solution,Result,Problem_Feature,Problem_Tags,Graph,EventProblem,Event
+admin.site.register(Problem)
+admin.site.register(Solution)
+admin.site.register(Result)
+admin.site.register(Problem_Feature)
+admin.site.register(Problem_Tags)
+admin.site.register(Graph)
+admin.site.register(Event)
+admin.site.register(EventProblem)
+
+
+
+
---
+++ media/solutions/26439842.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   sad
+   return 0;
+}
